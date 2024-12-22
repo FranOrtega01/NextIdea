ff35697889a8f232265032439d50d29d38e3b8b5
FranOrtega01 <franortega.wg@gmail.com>
2024-12-22 20:48:19 -0300

    commit

--- src/Database/firebase.js
+++ src/Database/firebase.js
@@ -1,16 +1,16 @@
 import { initializeApp } from "firebase/app";
 import {getFirestore} from 'firebase/firestore';
 import { getAuth} from "firebase/auth";
-import Constants from 'expo-constants'
+// import Constants from 'expo-constants'
 
 
 const firebaseConfig = {
-    apiKey: Constants.manifest.extra.apiKey,
-    authDomain: Constants.manifest.extra.authDomain,
-    projectId: Constants.manifest.extra.projectId,
-    storageBucket: Constants.manifest.extra.storageBucket,
-    messagingSenderId:Constants.manifest.extra.messagingSenderId,
-    appId:Constants.manifest.extra.appId
+    apiKey: Constants.expoConfig.extra.apiKey,
+    authDomain: Constants.expoConfig.extra.authDomain,
+    projectId: Constants.expoConfig.extra.projectId,
+    storageBucket: Constants.expoConfig.extra.storageBucket,
+    messagingSenderId:Constants.expoConfig.extra.messagingSenderId,
+    appId:Constants.expoConfig.extra.appId
 };
 
 // Initialize Firebase
